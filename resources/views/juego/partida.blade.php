f07f8986622ee3c8d85a01756f11a0a6273e38e6
ElMecatronico29 <carlitosmengam.29@gmail.com>
2023-04-12 17:26:40 -0400

    conflicto

--- resources/views/juego/partida.blade.php
+++ resources/views/juego/partida.blade.php
@@ -2,15 +2,11 @@
 @section('Titulo','Partidas')
 @section('contenido')
 
-<<<<<<< HEAD
     <div class="grid grid-cols-3 gap-3 sm:gap-6 m-4 ">        
         <div class="bg-blue-400 col-span-2 rounded h-96  flex items-center justify-center " id="mostrarMensaje"></div>
         <div> <h1 class="bg-blue-400 text-3xl text-center rounded h-96">Puntaje</h1></div>
     </div>
-    <script>
-=======
-    <script type="text/javascript">
->>>>>>> Jose
+    <script  type="text/javascript">
         function comprobar_jugadores_en_la_partida()
         {               
             $.ajax({     //comprueba si ya estan todos los jugadores para iniciar la partida
