3051d2eed334710a97292a50610304d19c12cff4
jotade13 <48698752+jotade13@users.noreply.github.com>
2023-04-15 18:47:56 -0400

    Update FotoController.php

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -20,7 +20,7 @@ class FotoController extends Controller
         ]);
 
         $nombreImagen = time().'.'.$request->file('imagen')->extension();
-        $request->file('imagen')->move(public_path(),$nombreImagen);
+        $request->file('imagen')->move(public_path("storage/imagenes"),$nombreImagen);
 
         Foto::create([
             'nombre' => $nombreImagen,
